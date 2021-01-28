7d7f7cf256b411cb29a9e8218d0cbcc0f3c01ee7
Jonata <jonata.passo@hotmail.com>
2021-01-28 12:04:39 -0300

    Crud Laravel e Vuejs

---
+++ resources/js/components/home.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="d-flex justify-content-center">
+        <div class="card col-md-11">
+            <div class="card-body">
+                <h3 class="card-title">CRUD LARAVEL E VUE.JS</h3>
+                <hr>
+                <h5 class="card-text">
+                    ## Sobre o Projeto <br><br>
+
+                    Projeto foi criado com os Frameworks Laravel e Vue.Js. <br><hr>
+
+                    Dependências do projeto: <br><br>
+
+                    - PHP 7.<br>
+                    - Vue.Js ^2.0.<br>
+                    - Laravel ^7.0.<br>
+                    - Composer ^2.0.<br>
+                    - Node ^10<br><hr>
+
+                    Ao baixar o projeto do GitHub deve-se executar os seguintes comandos no terminal:<br><br>
+
+                    - php composer.phar update (Caso não esteja instalado globalmente) <br>
+                    - npm install<br><hr>
+
+                    Foi utilizado o LARAVEL MIX, após baixar todas dependências executar o seguinte comando no terminal.<br><br>
+
+                    - npm run watch ( Caso seja necessário alterar algo no Front).<br><hr>
+
+                    ## Banco de dados<br><br>
+
+                    - MySql / MariaDB<br><br>
+
+                    Dump do banco na raiz do projeto, nome do arquivo (sellbrasil.sql)<hr>
+
+
+                    ## Servidor Web<br><br>
+
+                    Utilizo o Laragon como servidor porem pode ser utilizado qualquer outro LAMP.<br>
+
+                    Download - [Laragon](https://sourceforge.net/projects/laragon/files/releases/4.0/laragon-full.exe).<br><hr>
+
+                    ## GitHub<br><br>
+
+                    - https://github.com/JonatanPasso/crudlaravel<br>
+                </h5>
+                <hr>
+
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
